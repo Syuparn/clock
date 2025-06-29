9aaa4e5b66a4d0a87a7b81c572a8f61dd12fcd6f
syuparn <s.hello.spagetti@gmail.com>
2025-06-29 10:31:56 +0900

    add pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173
-  }
+  },
+  // HACK: fix link path because endpoint of GitHub pages is https://syuparn.github.io/clock/ (not root)
+  base: process.env.GITHUB_PAGES
+    ? ''
+    : './'
 })
